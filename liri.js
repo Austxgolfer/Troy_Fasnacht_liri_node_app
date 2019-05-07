8693ad60ca5ab7a80b2a85e48e2f164409fe1b6f
MAX <47061140+Austxgolfer@users.noreply.github.com>
2019-05-07 18:21:19 -0500

    added switch case to liri file

--- liri.js
+++ liri.js
@@ -2,4 +2,26 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 var omdbQuery = "http://www.omdbapi.com";
-var bitQuery = "http://www.artists.bandsintown.com/bandsintown-api"
+var bitQuery = "http://www.artists.bandsintown.com/bandsintown-api";
+
+var inquire = require("inquirer");
+
+inquire.prompt([
+    {
+        type:"checkbox",
+        message:"Search by one of the following.",
+        choices: ["Song","Band","Movie"],
+        name: "dbSelect",
+    }
+]).then(function(inquireresponse) {
+    switch (inquireresponse) {
+      case "Song":
+        console.log("Your answers were:" + inquireresponse.dbSelect);
+      break;
+      case "Band":
+        console.log (inquireresonse);
+      break;
+      case "Movie":
+        console.log (inquireresopnse);
+      break;    
+    }};
